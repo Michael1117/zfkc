28587f892b0ac6b4f339a631bcdebb2ab77a5a8b
Michael1117 <1322720377@qq.com>
2019-04-04 00:47:51 +0800

    捕获错误

--- public_course/6.test.js
+++ public_course/6.test.js
@@ -3,14 +3,16 @@
 
 new Promise((resolve, reject) => {
   setTimeout(()=> {
+      
     Math.random() < 0.5 ? resolve(100) :
     reject(-100)
   },1000)
+  throw new Error('ERROR')
 }).then(result => {
     console.log(result)
     //console.log(1)
 }, reason => {
-    console.log(reason)
+    console.log("NO " + reason)
     //console.log(2)
 })
 
