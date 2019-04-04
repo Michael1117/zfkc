1cfe10621c6ba1b69340cd3b4bf89cd1a00311ae
Michael1117 <1322720377@qq.com>
2019-04-04 15:09:43 +0800

    promise init

--- public_course/6.promise.js
+++ public_course/6.promise.js
@@ -1,73 +1,21 @@
 class Promise{
   constructor(excutorCallBack) {
-
     this.status = 'pending';
     this.value = undefined;
-    this.fulfilledAry = [];  // 成功要做的方法
-    this.rejectedAry = [];  // 失败要做的方法
 
-    // => 执行excutor (异常捕获)
+    // 执行excutor
     let resolveFn = result => {
-      let timer = setTimeout (() => {
-        clearTimeout(timer)
-        if(this.status !== 'pending') return
-        this.status = 'fulfilled';
-        this.value = result;
-
-        this.fulfilledAry.forEach(item => item(this.value))
-      }, 0)
-    };
+      if(this.status !== 'pending') return
+      this.status = 'fulfilled'
+      this.value = result;
+    }
 
     let rejectFn = reason => {
-      let timer = setTimeout (() => {
-        clearTimeout(timer);
-        if(this.status !== 'pending') return
-        this.status = 'rejected';
-        this.value = reason;
-        this.rejectedAry.forEach(item => item(this.value))
-      }, 0)
+      if(this.status !== 'pending') return 
+      this.status = 'rejected'
+      this.value = reason
     }
-
-    try{
-      excutorCallBack(resolveFn, rejectFn)
-    }catch(err){
-      // 有异常信息 按照rejected状态处理
-      rejectFn(err)
-    }
-  } 
-
-  //fulfilledCallBack成功的回调   rejectedCallBack失败的回调
-  then(fulfilledCallBack, rejectedCallBack) {
-    // 返回一个新的promise实例 
-    return new Promise((resolve, reject) => {
-      this.fulfilledAry.push(() => {
-        try{
-          //console.log(this.value)
-          //console.log(this)
-          let x = fulfilledCallBack(this.value)
-          x instanceof Promise ? x.then(resolve, reject): resolve(x) 
-         /*  if(x instanceof Promise) {
-            x.then(resolve, reject);
-            return;
-          }
-          resolve(x) */
-        }catch(err){
-          reject(err)
-        }
-      })
-
-      this.rejectedAry.push(() =>{
-        try{
-          let x = rejectedCallBack(this.value)
-          x instanceof Promise ? x.then(resolve, reject): resolve(x) 
-          //resolve(x)
-        }catch(err){
-          reject(err)
-        }
-      })
-    })
-    //this.fulfilledAry.push(fulfilledCallBack);
-    //this.rejectedAry.push(rejectedCallBack)
+    excutorCallBack(resolveFn, rejectFn);
   }
 }
 
--- public_course/6.test.js
+++ public_course/6.test.js
@@ -1,30 +1,19 @@
-let Promise = require('./6.promise');
+//let Promise = require('./6.promise');
 
 
-let p1 = new Promise((resolve, reject) => {
-  setTimeout(()=> {
-      
-    Math.random() < 0.5 ? resolve(100) :
-    reject(-100)
-  },1000)
+new Promise((resolve, reject)=> {
+    resolve(100);
+
+    reject(200)
+}).then(result => {
+    console.log(result)
+    console.log(1)
+    //return 1000
+    throw new Error('Error')
+}, () => {
+    console.log(2)
+}).then(result => {
+    console.log(result)
+}, reason=> {
+    console.log(reason)
 })
-
-console.log(p1 instanceof Promise)
-let p2 = p1.then(result => {
-   return result + 100
-}, reason => {
-    //throw new Error('new Error')
-    return reason + 100;
-})
-
-console.log(p2 instanceof Promise)
-/* let p3 = p2.then(result=> {
-    //console.log(p1 === p2) // false 执行then 返回的是一个新的 promise实例
-    //console.log(result)
-}, reason => {
-    //console.log(reason)
-})
- */
-//console.log(p3)
-
-//console.log(3)
